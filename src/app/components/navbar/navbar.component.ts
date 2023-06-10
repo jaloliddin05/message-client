bd3c5ba2f202eb1235ec21f4194bf14d1b995567
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-06-10 12:13:40 +0500

    feat: added navbar dropdawn

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -12,8 +13,12 @@ export class NavbarComponent {
   results: any[] = [];
   filteredResults: any[] = [];
   @Input() user: any;
+  filterIndex = 0;
 
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) {}
 
   filterResults() {
     if (this.searchTerm.trim() !== '') {
@@ -48,5 +53,41 @@ export class NavbarComponent {
     setTimeout(() => {
       this.showDropdown = false;
     }, 400);
+  }
+
+  handleKey(event: any) {
+    if (event.keyCode == 38) {
+      this.keyPgUp();
+    }
+    if (event.keyCode == 40) {
+      this.keyPgDn();
+    }
+    if (event.keyCode == 13) {
+      this.keyEnter();
+    }
+  }
+  keyPgUp() {
+    if (this.filterIndex > 0) {
+      this.filterIndex--;
+    }
+  }
+  keyPgDn() {
+    if (this.filterIndex < this.filteredResults.length) {
+      this.filterIndex++;
+    }
+  }
+
+  keyEnter() {
+    const user = this.filteredResults[this.filterIndex];
+    this.router.navigate(['home', 'single-user', user.name]);
+    this.onInputBlur();
+    this.searchTerm = user.name;
+  }
+
+  clickDropDown(index: number) {
+    const user = this.filteredResults[index];
+    this.router.navigate(['home', 'single-user', user.name]);
+    this.onInputBlur();
+    this.searchTerm = user.name;
   }
 }
